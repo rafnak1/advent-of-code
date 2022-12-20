733af8be29e87559bd63a84ed8645a1eb6dcfe0e
Rafael Nakata <rafaelk.nakata@gmail.com>
2022-12-19 22:37:24 -0300

    Adds input parser

--- day2/solution_part1.py
+++ day2/solution_part1.py
@@ -1,2 +1,8 @@
 def rock_paper_scissors_solve(raw_input: str) -> int:
     return 0
+
+def parse_input(raw_input: str) -> list[tuple[str, str]]:
+    stripped_input: str = raw_input.strip()
+    newline_split_input: list[str] = stripped_input.splitlines()
+    parsed_input: list[tuple[str, str]] = [(line[0], line[2]) for line in newline_split_input]
+    return parsed_input
--- day2/tests.py
+++ day2/tests.py
@@ -1,1 +1,20 @@
-import unittest
+import unittest, solution_part1
+
+class TestRockPaperScissorsSolve(unittest.TestCase):
+
+    @unittest.skip
+    def test_generic(self):
+        raw_input = "A Y\nB X\nC Z\n"
+        obtained: int = solution_part1.rock_paper_scissors_solve(raw_input=raw_input)
+        self.assertEqual(obtained, 15)
+
+class TestParseInput(unittest.TestCase):
+
+    def test_generic(self):
+        raw_input: str = "A X\nB X\nC Z\nB Z\n"
+        obtained: list[tuple[str, str]] = solution_part1.parse_input(raw_input=raw_input)
+        expected = [('A', 'X'), ('B', 'X'), ('C', 'Z'), ('B', 'Z')]
+        self.assertEqual(obtained, expected)
+
+if __name__ == '__main__':
+    unittest.main()
