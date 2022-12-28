3afbeb6dc2ac3722ff3f5ad98b5eb21f7de9590a
Rafael Nakata <rafaelk.nakata@gmail.com>
2022-12-28 15:58:34 -0300

    Day 2 part 1 completed

--- day2/tests.py
+++ day2/tests.py
@@ -2,7 +2,6 @@ import unittest, solution_part1
 
 class TestRockPaperScissorsSolve(unittest.TestCase):
 
-    @unittest.skip
     def test_generic(self):
         raw_input = "A Y\nB X\nC Z\n"
         obtained: int = solution_part1.rock_paper_scissors_solve(raw_input=raw_input)
@@ -15,6 +14,40 @@ class TestParseInput(unittest.TestCase):
         obtained: list[tuple[str, str]] = solution_part1.parse_input(raw_input=raw_input)
         expected = [('A', 'X'), ('B', 'X'), ('C', 'Z'), ('B', 'Z')]
         self.assertEqual(obtained, expected)
+
+class TestScoreForRound(unittest.TestCase):
+    
+    def test_A_Y(self):
+        round_data: tuple[str, str] = ('A', 'Y')
+        obtained: int = solution_part1.score_for_round(round_data=round_data)
+        self.assertEqual(obtained, 8)
+
+    def test_B_X(self):
+        round_data: tuple[str, str] = ('B', 'X')
+        obtained: int = solution_part1.score_for_round(round_data=round_data)
+        self.assertEqual(obtained, 1)
+
+    def test_C_Z(self):
+        round_data: tuple[str, str] = ('C', 'Z')
+        obtained: int = solution_part1.score_for_round(round_data=round_data)
+        self.assertEqual(obtained, 6)
+
+class TestOutcomeOfRound(unittest.TestCase):
+
+    def test_A_Y(self):
+        round_data: tuple[str, str] = ('A', 'Y')
+        obtained: str = solution_part1.outcome_of_round(round_data=round_data)
+        self.assertEqual(obtained, 'win')
+
+    def test_B_X(self):
+        round_data: tuple[str, str] = ('B', 'X')
+        obtained: str = solution_part1.outcome_of_round(round_data=round_data)
+        self.assertEqual(obtained, 'loss')
+
+    def test_C_Z(self):
+        round_data: tuple[str, str] = ('C', 'Z')
+        obtained: str = solution_part1.outcome_of_round(round_data=round_data)
+        self.assertEqual(obtained, 'draw')
 
 if __name__ == '__main__':
     unittest.main()
